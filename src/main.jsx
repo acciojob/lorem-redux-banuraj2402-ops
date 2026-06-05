58c0e5d833da333a7c2dcb4e5ebcdd725e4f70a0
Banu <banuraj2402@gmail.com>
2026-06-05 12:49:58 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App.jsx';
+import App from './components/App.js';
+import store from './store/store.js';
+
+createRoot(document.getElementById('root')).render
